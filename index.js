9f4f467e6ac362e80500604ac8302b4677b3faef
Michael <chunm@users.noreply.github.com>
2021-11-08 21:01:33 -0800

    Updated to meet Acceptance Criteria and allow user to choose output format
    In format 1 the program works as initially.
    
    In format 2: the output file has an extensionof .csv, the output file name is the title of the board without spaces, the content of the file is structured from left to right and is delimited by commas, and only content with at least 1 vote is included.

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const path = require('path');
 const { chromium } = require('playwright');
 const { exit } = require('process');
 
-const [url, file] = process.argv.slice(2);
+const [url, format, file] = process.argv.slice(2);
+var fileFormatName2;
 
 if (!url) {
     throw 'Please provide a URL as the first argument.';
+}
+if (format !== "1" && format !== "2") {
+    throw 'Please specify the format as 1 or 2 as the second argument.';
 }
 
 async function run() {
@@ -22,6 +26,19 @@ async function run() {
         throw 'Board title does not exist. Please check if provided URL is correct.'
     }
 
+    if(format === "1"){
+        if (!file) {
+            throw 'Please provide a file name as the third argument.'
+        }
+        return format1(page, boardTitle);
+    }else if(format === "2"){
+        fileFormatName2 = boardTitle.replace(/ /g, "") + ".csv";
+        return format2(page);
+    }
+
+}
+
+async function format1(page, boardTitle) {
     let parsedText = boardTitle + '\n\n';
 
     const columns = await page.$$('.easy-card-list');
@@ -47,7 +64,57 @@ async function run() {
     return parsedText;
 }
 
+async function format2(page) {
+    let parsedText = "";
+    let parsedTextArray = [];
+    let rows = 1;
+    
+    const columns = await page.$$('.easy-card-list');
+    const columnLength = columns.length;
+
+    for (let i = 0; i < columnLength; i++) {
+        parsedTextArray[i] = [];
+
+        const columnTitle = await columns[i].$eval('.column-header', (node) => node.innerText.trim());
+
+        if(columnTitle) {
+            parsedTextArray[i].push(columnTitle);
+        }else{
+            parsedTextArray[i].push("");
+        }
+
+        const messages = await columns[i].$$('.easy-board-front');
+        const messagesLength = messages.length;
+        if(messagesLength > rows) rows = messagesLength + 1;
+        for (let j = 0; j < messagesLength; j++) {
+            const messageText = await messages[j].$eval('.easy-card-main .easy-card-main-content .text', (node) => node.innerText.trim());
+            const votes = await messages[j].$eval('.easy-card-votes-container .easy-badge-votes', (node) => node.innerText.trim());
+            if(votes > 0) {
+                parsedTextArray[i].push(messageText);
+            }else{
+                parsedTextArray[i].push("");
+            }
+        }
+
+    }
+
+    for(let i = 0; i < rows; i++) {
+        for(let j = 0; j < columnLength; j++) {
+            if(parsedTextArray[j][i] !== "" && parsedTextArray[j][i] !== undefined){
+                parsedText += parsedTextArray[j][i];
+                if(j !== rows - 1){
+                    parsedText += ", ";
+                }
+            }
+        }
+        parsedText = parsedText.substring(0, parsedText.length - 2) + '\n';
+    }
+    
+    return parsedText;
+}
+
 function writeToFile(filePath, data) {
+    if(format === "2") filePath = fileFormatName2;
     const resolvedPath = path.resolve(filePath || `../${data.split('\n')[0].replace('/', '')}.txt`);
     fs.writeFile(resolvedPath, data, (error) => {
         if (error) {
